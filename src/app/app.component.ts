1372a6b6b534365a40b8a1b98933cb512d0b1113
PADMANABHUNI VEERA VENKATA MAHESH KUMAR <68265140+mahesh671@users.noreply.github.com>
2023-12-15 09:57:33 +0530

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import AOS styles
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pennant-learn-hub';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    AOS.init();
+  }
+  
 }
